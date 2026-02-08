42b8c6061d5b1c9b511dafa86d16e16f2a4231fd
Prashant Thakur <Prashant64bit@gmail.com>
2026-02-08 18:23:35 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,12 @@
 from telethon import TelegramClient, events, Button
 from config import apiID, apiHASH, botToken, ownerId
 
-client = TelegramClient("botsession", apiID, apiHASH)
+client = TelegramClient(
+    "botsession",
+    apiID,
+    apiHASH,
+    connection_retries=None,
+    retry_delay=5,
+    timeout=30,
+    flood_sleep_threshold=60
+)
